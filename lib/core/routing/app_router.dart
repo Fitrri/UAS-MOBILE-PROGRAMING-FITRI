c66ec61e0eb9d1985c027835c9667266b7da3fec
Fitrri <fffitrianisa2005@gmail.com>
2026-06-27 18:32:33 +0700

    daftarkan rute /profile ke konfigurasi gorouter

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
-// Ganti import lama ke file news_home_page.dart kita yang baru
 import '../../features/home/presentation/pages/news_home_page.dart';
+
+import '../../features/home/presentation/pages/profile_page.dart';
 
 class AppRouter {
   static final router = GoRouter(
@@ -8,8 +9,12 @@ class AppRouter {
     routes: [
       GoRoute(
         path: '/',
-        // Builder diubah dari HomePage() menjadi NewsHomePage()
         builder: (context, state) => const NewsHomePage(),
+      ),
+      
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) => const ProfilePage(),
       ),
     ],
   );
